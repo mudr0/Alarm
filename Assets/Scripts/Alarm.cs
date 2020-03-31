d40f9ebd599d61eceb2b5cc1b14bfa6486cc57ae
Lev <themostregular@gmail.com>
2020-03-31 13:24:04 +0700

    init

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    private AudioSource _alarmSound;
+
+    private void Start()
+    {
+        _alarmSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StopAllCoroutines();
+        StartCoroutine(RiseSound());
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutSound());
+    }
+
+    private IEnumerator RiseSound()
+    {
+        _alarmSound.Play();
+        while (_alarmSound.volume < 1)
+        {
+            _alarmSound.volume += 0.003f;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOutSound()
+    {
+        while (_alarmSound.volume > 0)
+        {
+            _alarmSound.volume -= 0.003f;
+            yield return null;
+        }
+        _alarmSound.Stop();
+
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        _animator.SetFloat("Speed", 0);
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move(false, 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(true, -1);
+        }
+    }
+
+    private void Move(bool xAnimationFlip, int direction)
+    {
+        _renderer.flipX = xAnimationFlip;
+        _animator.SetFloat("Speed", 1f);
+        transform.Translate(new Vector3(_speed * Time.deltaTime * direction, 0, 0));
+    }
+
+
+}
